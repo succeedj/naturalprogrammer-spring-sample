416a0b0d0627ecbd5d008dd60f5056c380837808
Succeed Jabangwe <succeedj@gmail.com>
2015-09-11 23:53:40 -0500

    Changed package name

--- src/main/java/com/naturalprogrammer/spring/sample/NaturalprogrammerSpringSampleApplication.java
+++ src/main/java/com/naturalprogrammer/spring/sample/NaturalprogrammerSpringSampleApplication.java
@@ -1,4 +1,4 @@
-package demo;
+package com.naturalprogrammer.spring.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
