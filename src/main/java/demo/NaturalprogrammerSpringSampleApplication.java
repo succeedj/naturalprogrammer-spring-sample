b4756f213d4c8a9a6496567018adbaa69ba419d7
Succeed Jabangwe <succeedj@gmail.com>
2015-09-11 23:45:16 -0500

    First Commit

---
+++ src/main/java/demo/NaturalprogrammerSpringSampleApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalprogrammerSpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaturalprogrammerSpringSampleApplication.class, args);
+    }
+}
